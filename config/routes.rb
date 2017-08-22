621fb249eefd3582778ebbaba3ad1948237e3458
Anais <anais.cuisinier@gmail.com>
2017-08-22 15:18:53 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/new'
+
+  get 'home/create'
+
+  get 'home/update'
+
+  get 'home/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
